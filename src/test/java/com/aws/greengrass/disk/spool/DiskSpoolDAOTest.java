416a28e9653ad43166b8b65702588e7ff56806aa
robcmann <111587784+robcmann@users.noreply.github.com>
2023-06-22 14:35:28 -0400

    feat: update for open sourcing (#25)

--- src/test/java/com/aws/greengrass/disk/spool/DiskSpoolDAOTest.java
+++ src/test/java/com/aws/greengrass/disk/spool/DiskSpoolDAOTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.aws.greengrass.disk.spool;
 
 import com.aws.greengrass.mqttclient.spool.SpoolMessage;
